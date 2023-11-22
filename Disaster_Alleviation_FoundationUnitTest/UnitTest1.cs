85d172b1f53ae7d9ff633bcb9c5657c2fc435fba
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2023-11-22 20:41:26 +0200

    update

---
+++ Disaster_Alleviation_FoundationUnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Disaster_Alleviation_FoundationUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
