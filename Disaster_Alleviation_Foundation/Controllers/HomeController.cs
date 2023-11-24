7b7bb3469b9d5f9d2e0a31540e4c4a6d8c259c24
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2023-11-24 03:14:18 +0200

    update_Part3

--- Disaster_Alleviation_Foundation/Controllers/HomeController.cs
+++ Disaster_Alleviation_Foundation/Controllers/HomeController.cs
@@ -1,21 +1,40 @@
-﻿using Disaster_Alleviation_Foundation.Models;
+﻿using Disaster_Alleviation_Foundation.Data;
+using Disaster_Alleviation_Foundation.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Disaster_Alleviation_Foundation.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var viewModel = new HomeViewModel
+            {
+                Disasters = await _context.Disaster.ToListAsync(),
+                MonetaryDonations = await _context.MonetaryDonation.ToListAsync(),
+                GoodsDonations = await _context.GoodsDonation.ToListAsync(),
+                MonetaryAllocation = await _context.MonetaryAllocation.ToListAsync(),
+                GoodsAllocation = await _context.GoodsAllocation.ToListAsync(),
+
+                // Calculate totals
+                TotalMoneyDonated = await _context.MonetaryDonation.SumAsync(m => m.Amount),
+                TotalGoodsDonated = await _context.GoodsDonation.SumAsync(g => g.ItemCount),
+                AvailableMoney = await _context.MonetaryDonation.SumAsync(m => m.Amount) - await _context.MonetaryAllocation.SumAsync(m => m.Amount),
+                AvailableGoods = await _context.GoodsDonation.SumAsync(g => g.ItemCount) - await _context.GoodsAllocation.SumAsync(g => g.ItemCount)
+            };
+
+
+            return View(viewModel); // Pass the viewModel to the view
         }
 
         public IActionResult Privacy()
--- Disaster_Alleviation_Foundation/Models/HomeViewModel.cs
+++ Disaster_Alleviation_Foundation/Models/HomeViewModel.cs
@@ -2,5 +2,17 @@
 {
     public class HomeViewModel
     {
+        public List<Disaster> Disasters { get; set; }
+        public List<MonetaryDonation> MonetaryDonations { get; set; }
+        public List<GoodsDonation> GoodsDonations { get; set; }
+
+        public List<GoodsAllocation> GoodsAllocation { get; set; }
+
+        public List<MonetaryAllocation> MonetaryAllocation { get; set; }
+
+        public int TotalMoneyDonated { get; set; }
+        public int TotalGoodsDonated { get; set; }
+        public int AvailableMoney { get; set; }
+        public int AvailableGoods { get; set; }
     }
 }
